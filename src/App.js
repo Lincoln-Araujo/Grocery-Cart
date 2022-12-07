afec70f65b847fcc09578c8f001dcae1158fbf95
Lincoln Araujo <72479081+Lincoln-Araujo@users.noreply.github.com>
2022-12-07 10:47:55 -0300

    inserting Logo.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import GroceryCart from './components/GroceryCart';
-//import Logo from './components/Logo';
+import Logo from './components/Logo';
 
 export default function App() {
   return (
     <div className="App">
-      
+      <Logo />
       <GroceryCart />
     </div>
   );
