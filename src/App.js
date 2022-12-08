f03c19026fc9680077f9029fa37f9c8abf250a54
Lincoln Araujo <lincoln.fa@hotmail.com>
2022-12-08 10:53:20 -0300

    updating files

--- src/App.js
+++ src/App.js
@@ -3,11 +3,9 @@ import GroceryCart from './components/GroceryCart';
 import Logo from './components/Logo';
 
 export default function App() {
-  fetch()
-    .then()
   return (
     <div className="App">
-      <Logo />
+      <Logo/>
       <GroceryCart />
     </div>
   );
